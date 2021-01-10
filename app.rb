415e40d9707e7d6fb55f6bbf48009fe6c8c60aae
imcarissa <carissa.sarreal@gmail.com>
2021-01-10 18:43:24 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Carissa"
+    end
+
+    get '/hometown' do
+        "My hometown is LA"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is whatever makes me cry"
+    end
 end
